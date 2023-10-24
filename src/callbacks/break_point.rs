a9376f5b626ff07735dc222357f0bcb6b226d440
Ebrecht Roland (IFAG CSS TI SW SWA) <Roland.Ebrecht@infineon.com>
2023-10-24 07:36:16 +0200

    Added stop on breakpoint

--- src/callbacks/break_point.rs
+++ src/callbacks/break_point.rs
@@ -2,6 +2,7 @@ use super::Unicorn;
 
 /// Hook for flash_load_img callback handling
 ///
-pub fn hook_code_breakpoint_callback<D>(_emu: &mut Unicorn<D>, _address: u64, _size: u32) {
+pub fn hook_code_breakpoint_callback<D>(emu: &mut Unicorn<D>, _address: u64, _size: u32) {
     println!("Breakpoint reached\n");
+    emu.emu_stop().unwrap();
 }
